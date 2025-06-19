3f7dc5485cd2f324446e624bc23bf48464584537
unknown <tejasvibhadrinath26@gmail.com>
2025-06-19 13:42:00 -0700

    data block removed from eks

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,17 +1,3 @@
-data "aws_eks_cluster" "cluster" {
-  name = var.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster_auth" {
-  name = var.cluster_name
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster_auth.token
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 16.0"
