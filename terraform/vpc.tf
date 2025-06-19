af83b7c74b9e513c3a0deaa86ca0f96b6afb6f78
unknown <tejasvibhadrinath26@gmail.com>
2025-06-19 13:26:59 -0700

    version of vpc changed

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.18.1"
+  version = "5.1.1"
 
   name                 = "${var.cluster_name}-vpc"
   cidr                 = var.vpc_cidr
