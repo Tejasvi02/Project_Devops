8c012e437dfbd80ccbd64476b5943505868adc3e
unknown <tejasvibhadrinath26@gmail.com>
2025-06-19 15:48:35 -0700

    changed to 16 and added provider

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,3 +1,22 @@
+provider "aws" {
+  region  = var.region
+  profile = "your-admin-profile" # Optional
+}
+
+data "aws_eks_cluster" "cluster" {
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
+}
+
+data "aws_eks_cluster_auth" "cluster_auth" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster_auth.token
+}
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 16.0"
