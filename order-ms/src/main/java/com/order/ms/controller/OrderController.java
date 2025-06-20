21f687110363b3e805d818e3ba201f1fbf1bfcbf
unknown <tejasvibhadrinath26@gmail.com>
2025-06-19 19:05:24 -0700

    controller health in order-ms

--- order-ms/src/main/java/com/order/ms/controller/OrderController.java
+++ order-ms/src/main/java/com/order/ms/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.order.ms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,11 @@ public class OrderController {
 
 	@Autowired
 	private KafkaTemplate<String, OrderEvent> kafkaTemplate;
+	
+	@GetMapping("/health")
+	public String HealthCheck() {
+		return "Success";
+	}
 
 	@PostMapping("/orders")
 	public void createOrder(@RequestBody CustomerOrder customerOrder) {
